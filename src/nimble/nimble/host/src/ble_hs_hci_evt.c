0b6ddeedd9a4dba7aecb07b28c8aa6dd482edd0e
DTTerastar <DT@Terastar.biz>
2025-11-10 05:59:02 -0500

    Fix pointer arithmetic in ble_hs_hci_evt_le_adv_rpt functions

--- src/nimble/nimble/host/src/ble_hs_hci_evt.c
+++ src/nimble/nimble/host/src/ble_hs_hci_evt.c
@@ -465,7 +465,7 @@ ble_hs_hci_evt_le_adv_rpt_first_pass(const void *data, unsigned int len)
         rpt = data;
 
         len -= sizeof(*rpt) + 1;
-        data += sizeof(rpt) + 1;
+        data += sizeof(*rpt) + 1;
 
         if (rpt->data_len > len) {
             return BLE_HS_ECONTROLLER;
@@ -506,7 +506,7 @@ ble_hs_hci_evt_le_adv_rpt(uint8_t subevent, const void *data, unsigned int len)
     for (i = 0; i < ev->num_reports; i++) {
         rpt = data;
 
-        data += sizeof(rpt) + rpt->data_len + 1;
+        data += sizeof(*rpt) + rpt->data_len + 1;
 
         desc.event_type = rpt->type;
         desc.addr.type = rpt->addr_type;
